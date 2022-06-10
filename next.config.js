74aeda7c6d7c5dbb379c4007ad99c5752ca5bf1d
Deniss Kozickis <deniss.kozickis@gmail.com>
2022-06-10 21:29:07 +0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    legacyBrowsers: false,
+    browsersListForSwc: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return <div>Ittty bitty tiny starter</div>;
+};
+
+export default Home;
